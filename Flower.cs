d803c6a81d81dc131c3be4b32bb325d079b8ba5e
LorexStaff <126458434+LorexStaff@users.noreply.github.com>
2023-11-04 23:17:34 +0700

    Add files via upload

--- Flower.cs
+++ Flower.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
-namespace hope
+namespace DataBase_LB3
 {
     public class Flower
     {
+        [Key]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Region { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
         public bool Rare { get; set; }
-
-        public Flower(string name, string region, int price, bool rare)
-        {
-            Name = name;
-            Region = region;
-            Price = price;
-            Rare = rare;
-        }
     }
 }
---
+++ FlowerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase_LB3
+{
+    class FlowerContext : DbContext
+    {
+        public FlowerContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<Flower> Flowers { get; set; }
+    }
+}
